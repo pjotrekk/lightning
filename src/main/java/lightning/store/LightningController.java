9bbfdb61d7d51faab7f73796d028081df6925315
Piotr Szuberski <piotr.szuberski@polidea.com>
2020-10-02 12:17:08 +0200

    Add controllers

---
+++ src/main/java/lightning/store/LightningController.java
@@ -0,0 +1,25 @@
+package lightning.store;
+
+import lightning.model.Lightning;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class LightningController {
+  private final LightningRepository repository;
+
+  @GetMapping("/lightnings")
+  public List<Lightning> getAllLightnings(@RequestParam(required = false) Long timestamp) {
+    if (timestamp != null) {
+      return repository.findByTimestampGreaterThan(timestamp);
+    }
+    return repository.findAll(Sort.by("timestamp").descending());
+  }
+
+}
